9f9c6f6916ea43bba80e1fd0b9296f82a4a10632
Sim-JuHeun <juheun9912@gmail.com>
2026-01-30 14:26:47 +0900

    초기설정 및 ignore 파일 생성

---
+++ HaruJapaneseWordApp/HaruJapaneseWordApp/HaruJapaneseWordAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaruJapaneseWordAppApp.swift
+//  HaruJapaneseWordApp
+//
+//  Created by 심주흔 on 1/30/26.
+//
+
+import SwiftUI
+
+@main
+struct HaruJapaneseWordAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
